05772ebec43b5ec991943b1b1f9dbe54db248ffd
Mason Reichbauer <mreichba@mail.usf.edu>
2020-02-03 12:09:40 -0500

    added template for dashboard

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-
+import './DashboardRoute.css'
 class DashboardRoute extends Component {
   render() {
     return (
-      <section>
-        implement and style me
+      <section className='dash'>
+        <h2>Dashboard</h2>]
+        <button>Start Practicing</button>
+        <h3>Words to Practice</h3>
+        <p>WE'LL DO A GET REQUEST HERE FOR WORDS IN DB ALONG WITH CORRECT AND INCORRECT GUESSES</p>
       </section>
     );
   }
