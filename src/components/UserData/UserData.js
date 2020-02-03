bcdd759ec8987cd68688a838161e13d03359a5a5
Mason Reichbauer <mgreichba@gmail.com>
2020-02-03 16:13:41 -0500

    added words/correct/incorrect

--- src/components/UserData/UserData.js
+++ src/components/UserData/UserData.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import  { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import UserContext from '../../contexts/UserContext';
 import './UserData.css'
 
@@ -7,11 +7,18 @@ export default class UserData extends Component {
     static contextType = UserContext
 
     render() {
+        const words = this.context.words.map((word, idx) => (
+            <li className='contact' key={idx}>
+                <span className='bold'></span>{word.original} | {word.correct_count} | {word.incorrect_count}
+            </li>
+        ))
         return (
             <div className="userData">
                 <h3 className="userData_lang">You are learning: {this.context.language}</h3>
                 <div className="wordBox">
-                    {/* word.original */} words go here
+                    <h4>Words to Practice</h4>
+                    <h5>Word | Correct | Incorrect</h5>
+                    {words}
                 </div>
                 <p className="userData_score">Total score for correct answers: {this.context.totalScore}</p>
                 <button className="startLearning_btn"><Link className="userData_link" to="/learn">Start Learning</Link></button>
