178fc6cec080de9e2bc31d729d75b0be968e4070
AustinT33 <austintumlinson@gmail.com>
2020-02-03 14:37:06 -0600

    solved display issues/context

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import LanguageApiService from '../../services/language-api-service';
-import LanguageContext from '../../contexts/LanguageContext';
+import UserContext from '../../contexts/UserContext';
 import UserData from '../../components/UserData/UserData';
 import './DashboardRoute.css'
 
 class DashboardRoute extends Component {
-  static contextType = LanguageContext;
+  static contextType = UserContext;
 
   componentDidMount() {
     LanguageApiService.getLanguage().then(res => {
       const { language, words } = res;
       this.context.setLanguage(language);
       this.context.setWords(words);
+      //console.log(language.name)
     });
   }
 
