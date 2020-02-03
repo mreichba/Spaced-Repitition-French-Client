8181be048f39289f6e50bf6893ea69267c1bae5d
Mason Reichbauer <mgreichba@gmail.com>
2020-02-03 12:10:23 -0500

    added template for dashboard

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -4,7 +4,7 @@ class DashboardRoute extends Component {
   render() {
     return (
       <section className='dash'>
-        <h2>Dashboard</h2>]
+        <h2>Dashboard</h2>
         <button>Start Practicing</button>
         <h3>Words to Practice</h3>
         <p>WE'LL DO A GET REQUEST HERE FOR WORDS IN DB ALONG WITH CORRECT AND INCORRECT GUESSES</p>
