04246ae177b1a8d6b8a3026ca11b99ecc45ecd9d
Mason Reichbauer <mreichba@mail.usf.edu>
2020-02-03 16:59:41 -0500

    modded answers columns

--- src/components/UserData/UserData.js
+++ src/components/UserData/UserData.js
@@ -10,13 +10,16 @@ export default class UserData extends Component {
         const words = this.context.words.map((word, idx) => (
             <ul className='wordUList' key={idx}>
                 <li className="wordName">{word.original}</li>
-                <li className="wordCorrect">{word.correct_count}</li>
-                <li className="wordIncorrect">{word.incorrect_count}</li>
+                <div className='answers'>
+                    <li className="wordCorrect">{word.correct_count}</li>
+                    <li className="wordIncorrect">{word.incorrect_count}</li>
+                </div>
             </ul>
         ))
         return (
             <div className="userData">
                 <h3 className="userData_lang">You are learning: {this.context.language}</h3>
+                <p className="userData_score">Total score for correct answers: {this.context.totalScore}</p>
                 <div className="wordBox">
                     <h4 className="wordHeader">Words</h4>
                     <h4 className="wordScores">Correct</h4>
@@ -25,7 +28,6 @@ export default class UserData extends Component {
                         {words}
                     </div>
                 </div>
-                <p className="userData_score">Total score for correct answers: {this.context.totalScore}</p>
                 <button className="startLearning_btn"><Link className="userData_link" to="/learn">Start Learning</Link></button>
             </div>
         )
