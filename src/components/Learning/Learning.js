411a38015ec879ee029f1b583e75e638b9d7ef8b
AustinT33 <austintumlinson@gmail.com>
2020-02-04 15:31:42 -0600

    debugging

---
+++ src/components/Learning/Learning.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react'
+import Button from '../Button/Button'
+import { Input, Label } from '../Form/Form'
+import LanguageApiService from '../../services/language-api-service';
+
+class Learning extends Component {
+    static correctIncorrect;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            guess: '',
+            answer: '',
+            isCorrect: '',
+            nextWord: this.props.head.nextWord,
+            currentWord: '',
+            totalScore: this.props.head.totalScore,
+            wordCorrectCount: this.props.head.wordCorrectCount,
+            wordIncorrectCount: this.props.head.wordIncorrectCount,
+            correctIncorect: '',
+            showinput: true
+        }
+    }
+
+    postGuessHandler = (e) => {
+        e.preventDefault();
+        const { guess } = this.state;
+        this.setState({
+            currentWord: this.state.nextWord
+        })
+        LanguageApiService.postGuess(guess).then(res => {
+            const {
+                answer,
+                isCorrect,
+                nextWord,
+                totalScore,
+                wordCorrectCount,
+                wordIncorrectCount,
+            } = res;
+
+            this.correctIncorrect = this.rightWrong(res.isCorrect);
+
+            this.setState({
+                answer,
+                isCorrect,
+                nextWord,
+                totalScore,
+                wordCorrectCount,
+                wordIncorrectCount
+            });
+        });
+        this.setState({ showinput: false });
+    };
+
+    handleNextClick = (e) => {
+        LanguageApiService.getHead().then(res => {
+            const { wordCorrectCount, wordIncorrectCount } = res;
+            this.setState({ wordCorrectCount, wordIncorrectCount });
+        })
+        this.setState({ 
+            showinput: true,
+            answer: ''
+        });
+    };
+
+    rightWrong = answer => {
+        if (answer) {
+            return (
+                <h2 className='answerh2'>Correct!</h2>
+            );
+        } else {
+            return (
+                <h2 className="answerh2">Incorrect..</h2>
+            );
+        }
+    };
+
+    updateGuess(guess) {
+        this.setState({ guess });
+    }
+
+    render() {
+        return (
+            <div className="learning">
+                {' '}
+                <div className="learning_stats">
+                    <h4 className="learning_correct">
+                        You have answered this word correctly {this.state.wordCorrectCount} times.
+                    </h4>
+                    <h4 className="learning_incorrect">
+                        You have answered this word incorrectly {this.state.wordIncorrectCount} times.
+                    </h4>
+                </div>
+                <div className="learning_total_score">
+                    <main classname="displayScore">
+                        <p>Your total score is: {this.state.totalScore}</p>
+                    </main>
+                </div>
+                <div className="learning_prompt">
+                    <h2>Translate the word: </h2>
+                    <span className="learning_word">{this.state.nextWord}</span>
+                </div>
+                <div className="displayFeedback">
+                    {this.state.answer && (
+                        <div>
+                            <p className="learning_guess">
+                                {`The correct translation for ${this.state.currentWord} was ${this.state.answer} and you chose ${this.state.guess}!`}
+                            </p>
+                            <Button type="submit" className="try_another" onClick={e => this.handleNextClick(e)}>
+                                Try another word!
+                            </Button>
+                        </div>
+                    )}
+                </div>
+                {this.state.showinput && (
+                    <form
+                    className="learning_answer_form"
+                    onSubmit={e => this.postGuessHandler(e)}
+                    >
+                        <Label 
+                            className="learning_answer_form_label"
+                            htmlFor="learn_guess_input"
+                        >
+                            What's the translation for this word?
+                        </Label>
+                        <div className="learning_answer_form_inputs">
+                            <Input
+                            id="learn_guess_input"
+                            type="text"
+                            required
+                            onChange={e => this.updateGuess(e.target.value)}
+                            />
+                            <Button type="submit">Submit your answer</Button>
+                        </div>
+                    </form>
+                )}
+                {!this.state.showinput && this.correctIncorrect}
+            </div>
+        )
+    }
+}
+export default Learning;
