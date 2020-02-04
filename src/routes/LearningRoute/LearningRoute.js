3dc02a78c0a49b8b42605df3b7ceba8ff7446cf0
AustinT33 <austintumlinson@gmail.com>
2020-02-04 09:47:26 -0600

    working on learn word

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import UserContext from '../../contexts/UserContext'
 import './LearningRoute.css'
 
 class LearningRoute extends Component {
+  static contextType = UserContext;
   render() {
+
+    const words = this.context.words.map((word, idx) => (
+      <ul className='wordContainer' key={idx}>
+          <li className="currentWord">{word.original}</li>
+          <li className="wordCorrect_count">{word.correct_count}</li>
+          <li className="wordIncorrect_count">{word.incorrect_count}</li>
+      </ul>
+    ))
+
     return (
-      <section className='learnSec'>
+      <section className="learnSec">
         <h2>Language Practice</h2>
+        <h4 className="learn_display">{words[0]}</h4>
       </section>
     );
   }
