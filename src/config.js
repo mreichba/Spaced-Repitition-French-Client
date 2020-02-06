9a07efe0b1fa90610c230a898143d1ce88a6f647
Mason Reichbauer <mreichba@mail.usf.edu>
2020-02-06 11:53:39 -0500

    fixed cy tests

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
+//production config settings
 export default {
-  TOKEN_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api'
+  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
+  TOKEN_KEY: process.env.REACT_APP_API_KEY
+  //  **USE THIS TOKEN KEY FOR CYPRESS TESTS**
+  //   TOKEN_KEY: 'blogful-client-auth-token'
+
 }
+
+
