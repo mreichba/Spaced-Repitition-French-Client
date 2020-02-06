70340f4cc9a9414db26e9ad89db418af19056824
AustinT33 <austintumlinson@gmail.com>
2020-02-06 11:51:57 -0600

    added logo and style adjustments

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
+import Logo from '../../Logo/spaced-rep-logo.png'
+import './RegistrationRoute.css'
 
 class RegistrationRoute extends Component {
   static defaultProps = {
@@ -16,6 +18,7 @@ class RegistrationRoute extends Component {
   render() {
     return (
       <section className='reg'>
+        <img src={Logo} className="spaced-logo"alt="spaced-rep-logo"/>
         <p>
           Practice learning a language with the spaced repetition revision technique.
         </p>
